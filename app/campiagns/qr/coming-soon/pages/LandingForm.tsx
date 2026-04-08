bc7e5a4ed86af62155d3362631cc7455cf825cb8
RAjni Kumari <rajnidss066@gmail.com>
2026-04-08 11:18:20 +0530

    Validation change

--- app/campiagns/qr/coming-soon/pages/LandingForm.tsx
+++ app/campiagns/qr/coming-soon/pages/LandingForm.tsx
@@ -37,7 +37,7 @@ export default function LandingForm() {
     setFormData((prev) => ({
       ...prev,
       area: areaParam,
-      source: sourceParam === "qr" ? "qr" : "direct",
+      source: sourceParam === "qr" ? "qr" : "link",
     }));
 
     setArea(areaParam);
